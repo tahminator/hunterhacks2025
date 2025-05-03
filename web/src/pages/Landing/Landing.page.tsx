6507f4ecd40fd562630d80ef0c5a35b70e0e71f9
Daniel Sooknanan <daniel.sooknanan216@gmail.com>
2025-05-03 00:52:27 -0400

    Add background landing text

--- web/src/pages/Landing/Landing.page.tsx
+++ web/src/pages/Landing/Landing.page.tsx
@@ -1,5 +1,46 @@
-function LandingPage() {
-    return <></>
+import { Title, Stack, Text, Group, Box } from '@mantine/core'
+
+interface TextCutoutBackgroundProps {
+  textArray: string[]
 }
 
-export default LandingPage;
+function TextCutoutBackground({ textArray }: TextCutoutBackgroundProps) {
+  return (
+    <Box>
+      <Group w={'100%'} justify="flex-end" mb="sm">
+        <Text size="lg">I'm allergic to</Text>
+      </Group>
+      <Stack align="flex-end" gap={'2rem'}>
+        {textArray.map((text, index) => (
+          <Title
+            key={index}
+            style={{
+              fontSize: '3.5rem',
+            }}
+          >
+            {text}
+          </Title>
+        ))}
+      </Stack>
+    </Box>
+  )
+}
+
+function LandingPage() {
+  const allergens = [
+    'Gluten',
+    'Peanuts',
+    'Pistachios',
+    'Corn',
+    'Soy',
+    'Shellfish',
+    'Avocados',
+  ]
+  return (
+    <>
+      <TextCutoutBackground textArray={allergens} />
+    </>
+  )
+}
+
+export default LandingPage
