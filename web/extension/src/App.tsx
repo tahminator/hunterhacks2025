2d254aa2ab1b71eef553c597087b496c70e7791a
Daniel Sooknanan <daniel.sooknanan216@gmail.com>
2025-05-02 23:36:45 -0400

    Setup extension manifest

--- web/extension/src/App.tsx
+++ web/extension/src/App.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import '@mantine/core/styles.css';
+import './App.module.css'
 
 import { MantineProvider } from '@mantine/core';
 
