1543c1d1ba2d08cadc9f7617a448a9ebded83bdd
Daniel Sooknanan <daniel.sooknanan216@gmail.com>
2025-05-03 09:22:22 -0400

    Add right-click context menu

--- web/src/snipping/background.tsx
+++ web/src/snipping/background.tsx
@@ -1,7 +1,22 @@
-chrome.action.onClicked.addListener((tab) => {
-  console.log('clicked')
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id! },
-    files: ['snipping.js'],
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: 'AllerFreeContextMenuOption',
+    title: 'Aller-Free',
+    contexts: ['all'],
   })
+})
+
+// Add listener for when the menu item is clicked
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (!tab?.id) {
+    return
+  }
+
+  if (info.menuItemId === 'AllerFreeContextMenuOption') {
+    console.log('Context menu option clicked!')
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id! },
+      files: ['snipping.js'],
+    })
+  }
 })
